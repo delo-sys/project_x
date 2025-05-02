d31fa20e4c93cde456494602d73ef5e32ed7953a
delo-sys <mrodhiambo226@gmail.com>
2025-05-02 14:25:32 +0300

    projectx

---
+++ coded/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container my-5">
+        <div.class="mb-3">
+            <label class="form-label">First name</label>
+            <input type="email" class="form-control" id="Enter your first name" placeholder="
+        </div>
+        <div class="container my-5">
+        <div.class="mb-3">
+            <label class="form-label">Middle name</label>
+            <input type="email" class="form-control" id="Enter your first name" placeholder="
+        </div>
+        <div class="container my-5">
+        <div.class="mb-3">
+            <label class="form-label">Last name</label>
+            <input type="email" class="form-control" id="Enter your last name" placeholder="
+        </div>
+        <div class="container my-5">
+        <div.class="mb-3">
+            <label class="form-label">Email</label>
+            <input type="email" class="form-control" id="Enter your first name" placeholder="
+        </div>
+        <div class="container my-5">
+        <div.class="mb-3">
+            <label class="form-label">mobile number</label>
+            <input type="email" class="form-control" id="Enter your number" placeholder="
+        </div>
+        <button class="btn btn_dark btn-lg my-3">submit</button>
+    </div>
+</body>
+</html>
